bd18196485116fe8c54c89d5719ddcf4809136e0
Piyush1803 <piyushdhanwal2003@gmail.com>
2026-01-16 00:01:16 +0530

    adding ts file to upload images

---
+++ src/assets/companies/index.ts
@@ -0,0 +1,37 @@
+// Import all company logo images for proper Vite bundling
+import company1 from './company1.png';
+import company2 from './company2.png';
+import company3 from './company3.png';
+import company4 from './company4.png';
+import company5 from './company5.png';
+import company6 from './company6.png';
+import company7 from './company7.jpeg';
+import company8 from './company8.png';
+import company9 from './company9.png';
+import company10 from './company10.png';
+
+export const companyLogos = [
+  company1,
+  company2,
+  company3,
+  company4,
+  company5,
+  company6,
+  company7,
+  company8,
+  company9,
+  company10,
+];
+
+export {
+  company1,
+  company2,
+  company3,
+  company4,
+  company5,
+  company6,
+  company7,
+  company8,
+  company9,
+  company10,
+};
